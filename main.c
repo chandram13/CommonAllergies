14289a4f89564f65e4522ee470ae57b51a6dba1f
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-29 08:27:32 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,43 @@
+// Marvish Chandra
+#include <stdio.h>
+
+public static void foodAllergies(string[] food){
+	if food == "":
+	printf(".")
+	if food == "":
+	printf(".")
+	if food == "":
+	printf(".")
+	else: printf(".")
+    return 0;
+}
+public static void animalAllergies(string[] animal){
+	if animal == "":
+	printf(".")
+	if animal == "":
+    printf(".")
+	else: printf(".")
+    return 0;
+}
+public static void pollen(string[] outdoorpollen){
+
+}
+public static void mold(string[] mold){
+
+}
+public static void termites(string[] termites){
+
+}
+
+public static void medications(string[] medicine){
+
+}
+public static void latexMaterial(string[] latex){
+
+}
+public static void insectStings(string[] insects){
+
+}
+public static void householdChemicals(string[] perfumes){
+
+}
